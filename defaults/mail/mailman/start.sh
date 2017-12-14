d55895283705039deb80df427d9b1126b248411e
Matias Kreder <mkreder@gmail.com>
2017-12-13 23:08:26 -0300

    fixes for mailman

--- defaults/mail/mailman/start.sh
+++ defaults/mail/mailman/start.sh
@@ -14,7 +14,7 @@ chmod +x /etc/mailman/add_lists.sh
 /etc/mailman/add_lists.sh
 > /etc/mailman/add_lists.sh
 /usr/lib/mailman/bin/genaliases  > /var/lib/mailman/data/virtual-mailman
-/usr/lib/mailman/bin/mailmanctl start
+/usr/lib/mailman/bin/mailmanctl -s start
 
 
 while true ; do
